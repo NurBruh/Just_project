1730840653f1feb59a67766dd1842736074d0c61
NurBruh <amdfx9590@bk.ru>
2025-05-16 23:49:52 +0500

    bruh

---
+++ Just_project.Admin/Models/UpdateBlogViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Just_project.Admin.Models
+{
+    public class UpdateBlogViewModel
+    {
+        public int Id { get; set; }
+        public string Title_en { get; set; }
+        public string Description_en { get; set; }
+
+        public string Title_ru { get; set; }
+        public string Description_ru { get; set; }
+
+        public string Title_kk { get; set; }
+        public string Description_kk { get; set; }
+
+        public IFormFile? NewImage { get; set; }
+        
+    }
+}
---
+++ Just_project.Admin/Models/UpdateComplistViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Just_project.Admin.Models
+{
+    public class UpdateComplistViewModel
+    {
+        public int Id { get; set; }
+        public string Title_en { get; set; }
+        public string Title_ru { get; set; }
+        public string Title_kk { get; set; }
+        public IFormFile? NewImage { get; set; }
+
+    }
+}
